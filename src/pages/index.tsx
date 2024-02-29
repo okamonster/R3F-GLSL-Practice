96e53823af464524e9bde3319c22fabd65aaf632
okamonster <okamonster0615@gmail.com>
2024-03-01 03:19:17 +0900

    feat:add plane

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
-import { Inter } from 'next/font/google'
+/* eslint-disable react/no-unknown-property */
+import { useEffect } from 'react'
+
+import { OrthographicCamera } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
 import Head from 'next/head'
-import Image from 'next/image'
 
 import { DefaultLayout } from '~/layouts/DefaultLayout'
-import styles from '~/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import firstFragmentShader from '~/shaders/firstShader/fragment.glsl'
+import firstVertexShader from '~/shaders/firstShader/vertex.glsl'
 
 export default function Home() {
   return (
@@ -17,102 +19,42 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <DefaultLayout>
-        <main className={`${styles.main} ${inter.className}`}>
-          <div className={styles.description}>
-            <p>
-              Get started by editing&nbsp;
-              <code className={styles.code}>src/pages/index.tsx</code>
-            </p>
-            <div>
-              <a
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                By{' '}
-                <Image
-                  src="/vercel.svg"
-                  alt="Vercel Logo"
-                  className={styles.vercelLogo}
-                  width={100}
-                  height={24}
-                  priority
-                />
-              </a>
-            </div>
-          </div>
-
-          <div className={styles.center}>
-            <Image
-              className={styles.logo}
-              src="/next.svg"
-              alt="Next.js Logo"
-              width={180}
-              height={37}
-              priority
-            />
-          </div>
-
-          <div className={styles.grid}>
-            <a
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              className={styles.card}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2>
-                Docs <span>-&gt;</span>
-              </h2>
-              <p>
-                Find in-depth information about Next.js features and&nbsp;API.
-              </p>
-            </a>
-
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              className={styles.card}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2>
-                Learn <span>-&gt;</span>
-              </h2>
-              <p>
-                Learn about Next.js in an interactive course with&nbsp;quizzes!
-              </p>
-            </a>
-
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              className={styles.card}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2>
-                Templates <span>-&gt;</span>
-              </h2>
-              <p>
-                Discover and deploy boilerplate example Next.js&nbsp;projects.
-              </p>
-            </a>
-
-            <a
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              className={styles.card}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2>
-                Deploy <span>-&gt;</span>
-              </h2>
-              <p>
-                Instantly deploy your Next.js site to a shareable URL
-                with&nbsp;Vercel.
-              </p>
-            </a>
-          </div>
-        </main>
+        <Canvas>
+          <ambientLight />
+          <Plane />
+          <OrthographicCamera position={[-0.5, 0.5, 0.5]} near={0.1} far={10} />
+        </Canvas>
       </DefaultLayout>
     </>
+  )
+}
+
+const Plane = () => {
+  const uniforms = {
+    u_time: { value: 0.0 },
+    u_mouse: { value: { x: 0.0, y: 0.0 } },
+    u_resolution: { value: { x: 0.0, y: 0.0 } },
+  }
+
+  useEffect(() => {
+    if (uniforms.u_resolution !== undefined) {
+      uniforms.u_resolution.value.x = window.innerWidth
+      uniforms.u_resolution.value.y = window.innerHeight
+    }
+  }, [uniforms.u_resolution])
+
+  useFrame(({ clock }) => {
+    uniforms.u_time.value = clock.getElapsedTime()
+  })
+
+  return (
+    <mesh>
+      <planeGeometry args={[2, 2]} />
+      <shaderMaterial
+        vertexShader={firstVertexShader}
+        fragmentShader={firstFragmentShader}
+        uniforms={uniforms}
+      />
+    </mesh>
   )
 }
