714f612917204aaf4bdec022b9d675cff753c22c
okamonster <okamonster0615@gmail.com>
2024-03-02 03:59:19 +0900

    feat:uv positon uv

--- src/shaders/blendColorShader/fragment.glsl
+++ src/shaders/blendColorShader/fragment.glsl
@@ -1,8 +1,10 @@
 uniform vec2 u_resolution;
 uniform float u_time;
+varying vec2 v_uv;
+varying vec3 v_position;
 
 void main() {
-    vec2 uv = gl_FragCoord.xy / 1324.0;
-    vec3 color = mix(vec3((sin(u_time*3.0)+1.0)/2.0,0.0,0.0),vec3(0.0,0.0,(cos(u_time)+1.0)/2.0),uv.x);
+    vec2 uv = gl_FragCoord.xy/u_resolution.xy;
+    vec3 color = vec3(v_position.x+1.0, v_position.y+1.0, 0.0);
     gl_FragColor = vec4(color, 1.0);
 }
