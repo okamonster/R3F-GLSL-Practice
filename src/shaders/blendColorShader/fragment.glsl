4a419a460bf1e25ae6f91d5d1f329d3c74e87480
okamonster <okamonster0615@gmail.com>
2024-03-02 03:33:06 +0900

    fix:mix color

--- src/shaders/blendColorShader/fragment.glsl
+++ src/shaders/blendColorShader/fragment.glsl
@@ -1,9 +1,8 @@
-uniform vec2 u_mouse;
 uniform vec2 u_resolution;
-uniform vec3 u_color;
 uniform float u_time;
 
 void main() {
-    vec3 color = vec3((sin(u_time*10.0)+1.0)/2.0,0,0);
-    gl_FragColor = vec4(color, 1.0).rgba;
+    vec2 uv = gl_FragCoord.xy / 1324.0;
+    vec3 color = mix(vec3((sin(u_time*3.0)+1.0)/2.0,0.0,0.0),vec3(0.0,0.0,(cos(u_time)+1.0)/2.0),uv.x);
+    gl_FragColor = vec4(color, 1.0);
 }
