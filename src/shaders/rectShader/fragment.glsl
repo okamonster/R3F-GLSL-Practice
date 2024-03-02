8ed70e44665c69454d3742f91b433847f608f5ec
okamonster <okamonster0615@gmail.com>
2024-03-03 04:40:43 +0900

    fix:Moving Rect

--- src/shaders/rectShader/fragment.glsl
+++ src/shaders/rectShader/fragment.glsl
@@ -9,10 +9,18 @@ float rect(vec2 pt, vec2 size, vec2 center) {
     return horz * vert;
 }
 
+uniform float u_time;
+
 void main() {
     float inSquere1 = rect(v_position.xy, vec2(0.3), vec2(-0.5,0.0));
     float inSquere2 = rect(v_position.xy, vec2(0.3), vec2(0.5,0.0));
-    vec3 color = vec3(1.0,1.0,0.0)*inSquere1 + vec3(0.0,1.0,0.0)*inSquere2;
+    
+    float radius = 0.2;
+    float time = u_time *3.0;
+    vec2 center = vec2(cos(time)*radius, sin(time)*radius);
+
+    float inRect = rect(v_position.xy, vec2(1.0), center);
+    vec3 color = vec3(1.0,1.0,0.0)*inRect;
 
     gl_FragColor = vec4(color,1.0);
 }
